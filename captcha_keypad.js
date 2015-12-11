701d970f96758ec32392d7f55ce745a54243dc6f
Marcelo Vani <marcellovani@yahoo.co.uk>
2015-12-11 22:58:45 +0000

    Added validation message, fixed styling on form error

--- captcha_keypad.js
+++ captcha_keypad.js
@@ -3,19 +3,31 @@
 
         attach: function (data, settings) {
             jQuery(document).ready(function () {
+                jQuery('#edit-captcha-keypad-input').keyup(function() {
+                    jQuery('#edit-captcha-keypad-input').val('');
+                    jQuery('#edit-captcha-keypad .message').css('color', 'red');
+                    jQuery('#edit-captcha-keypad .message').html(Drupal.t('Use keypad ->'));
+                });
+
                 jQuery('.form-item-captcha-keypad-input').append(
-                    '<span class="clear">' + Drupal.t('Clear') + '</span>'
+                    '<span class="clear">' + Drupal.t('Clear') + '</span><br/>' +
+                    '<span class="message"></span>'
                 );
 
                 jQuery('.form-item-captcha-keypad-input .clear').click(function () {
                     jQuery('#edit-captcha-keypad-input').val('');
+                    jQuery('#edit-captcha-keypad .message').html('');
                 });
 
                 jQuery('.captcha-keypad .inner span').click(function () {
                     var value = jQuery('#edit-captcha-keypad-input').val()
                     jQuery('#edit-captcha-keypad-input').val(value + jQuery(this).text());
+                    jQuery('#edit-captcha-keypad .message').html('');
                 });
             })
+        },
+        clean: function (data, settings) {
+            jQuery('#edit-captcha-keypad-input').val('zz');
         }
     };
 })(jQuery);
